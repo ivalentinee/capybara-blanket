0e9f4f6421fc4366292875611ecd05d1fb4cd9ec
Emperor <valentine.emperor@gmail.com>
2014-05-23 12:58:46 +0400

    a work around for no page given

--- lib/capybara/blanket.rb
+++ lib/capybara/blanket.rb
@@ -20,20 +20,16 @@ module Capybara
         self.coverage_data.send(*args)
       end
 
-      # Grab code coverage from the frontend
-      # Currently this adds something like a few second or more to every scenario
-      # The waits are lame but here's what it's trying to avoid
-      #    unknown error: You must call blanket.setupCoverage() first.
-      #       (Session info: chrome=31.0.1650.63)
-      #       (Driver info: chromedriver=2.6.232908,platform=Mac OS X 10.9.1 x86_64) (Selenium::WebDriver::Error::UnknownError)
       def extract_from page
-        @page = page
-        sleep(0.2) until coverage_is_setup?
-        page.evaluate_script("blanket.onTestsDone();")
-        sleep(0.2) until data_ready?
-        page_data = page.evaluate_script("window.CAPYBARA_BLANKET")
-        @@coverage_data.accrue! page_data
-        return page_data
+        if page
+          @page = page
+          sleep(0.2) until coverage_is_setup?
+          page.evaluate_script("blanket.onTestsDone();")
+          sleep(0.2) until data_ready?
+          page_data = page.evaluate_script("window.CAPYBARA_BLANKET")
+          @@coverage_data.accrue! page_data
+          return page_data
+        end
       end
 
       def coverage_is_setup?
