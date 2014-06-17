094a3a717f44d4cc91d148a0b5f4c2466ba8ef02
Emperor <valentine.emperor@gmail.com>
2014-06-17 15:31:36 +0400

    moved coverage method to coverage_data

--- spec/lib/capybara/blanket/coverage_data_spec.rb
+++ spec/lib/capybara/blanket/coverage_data_spec.rb
@@ -61,10 +61,16 @@ describe Capybara::Blanket::CoverageData do
     end
   end
 
-  describe "#percent_covered" do
+  describe "#file_coverage" do
     it "returns the percent covered for a given script" do
-      res = covdata.percent_covered covdata.files.keys[1]
+      res = covdata.file_coverage covdata.files.keys[1]
       expect( res ).to eq 60.0
+    end
+  end
+
+  describe "#coverage" do
+    it "returns total percent coverage of known lines of code as float" do
+      expect( covdata.coverage ).to eq 75.0
     end
   end
 end
